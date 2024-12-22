355af65435221c076e8479e9675b1fa62e401868
byeol <kimbyul5633@gmail.com>
2024-12-23 08:18:43 +0900

    024.12.23

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   let [글제목,b]=useState('여자 코트 추천');
-let a = '역삼 우동 맛집'
+  let[content, setContent] = useState(['남자코트 추천','강남 우동맛집','파이썬독학']);
+  let[like, likes]=useState(0);
   return (
     <div>
-    <div className="black-nav">
-      <h4>reactLogo</h4>
+      <div className="black-nav">
+        <h4>reactLogo</h4>
+      </div>
+    <div className="list">
+      <h4>{content[0]}<span onClick={()=>{likes(like+1)}}>❤️</span> {like} </h4>
+      <p>2월 17일 발행</p>
     </div>
-  <div className="list">
-    <h4>글제목</h4>
-    <p>2월 17일 발행</p>
-  </div>
+    <div className="list">
+      <h4>{content[1]}</h4>
+      <p>2월 17일 발행</p>
+    </div>
+    <div className="list">
+      <h4>{content[2]}</h4>
+      <p>2월 17일 발행</p>
+    </div>
   </div>
   )
 }
